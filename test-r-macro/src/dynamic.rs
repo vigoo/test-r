373d57cf9dedf66b1be17808bb918d79ca64f574
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-05-31 19:51:26 +0200

    Splitted the macro code into submodules

---
+++ test-r-macro/src/dynamic.rs
@@ -0,0 +1,124 @@
+use crate::deps::get_dependency_params_for_closure;
+use proc_macro::TokenStream;
+use proc_macro2::Ident;
+use quote::{ToTokens, quote};
+use syn::punctuated::Punctuated;
+use syn::{Expr, ExprClosure, ItemFn, Token, parse_macro_input, parse2};
+
+pub fn test_gen(item: TokenStream) -> TokenStream {
+    let ast: ItemFn = syn::parse(item).expect("test generator ast");
+    let generator_name = ast.sig.ident.clone();
+    let generator_name_str = generator_name.to_string();
+
+    let is_ignored = ast.attrs.iter().any(|attr| attr.path().is_ident("ignore"));
+
+    let register_ident = Ident::new(
+        &format!("test_r_register_generator_{}", generator_name_str),
+        generator_name.span(),
+    );
+
+    let is_async = ast.sig.asyncness.is_some();
+
+    let register_call = if is_async {
+        quote! {
+              test_r::core::register_test_generator(
+                  #generator_name_str,
+                  module_path!(),
+                  #is_ignored,
+                  test_r::core::TestGeneratorFunction::Async(std::sync::Arc::new(|| Box::pin(async move { #generator_name().await })))
+              );
+        }
+    } else {
+        quote! {
+            test_r::core::register_test_generator(
+                #generator_name_str,
+                module_path!(),
+                #is_ignored,
+                test_r::core::TestGeneratorFunction::Sync(std::sync::Arc::new(|| #generator_name()))
+            );
+        }
+    };
+
+    let wrapped_ast = if is_async {
+        quote! {
+            async fn #generator_name() -> Vec<test_r::core::GeneratedTest> {
+                let mut tests = test_r::core::DynamicTestRegistration::new();
+                #ast
+                #generator_name(&mut tests).await;
+                tests.to_vec()
+            }
+        }
+    } else {
+        quote! {
+            fn #generator_name() -> Vec<test_r::core::GeneratedTest> {
+                let mut tests = test_r::core::DynamicTestRegistration::new();
+                #ast
+                #generator_name(&mut tests);
+                tests.to_vec()
+            }
+        }
+    };
+
+    let result = quote! {
+        #[cfg(test)]
+        #[test_r::ctor::ctor(crate_path=::test_r::ctor)]
+        fn #register_ident() {
+             #register_call
+        }
+
+        #wrapped_ast
+    };
+
+    result.into()
+}
+
+pub fn add_test(input: TokenStream) -> TokenStream {
+    let params = parse_macro_input!(input with Punctuated::<Expr, Token![,]>::parse_terminated);
+
+    if params.len() != 4 {
+        panic!("add_test! expects exactly 4 parameters");
+    }
+
+    let dtr_expr = &params[0];
+    let name_expr = &params[1];
+    let test_props_expr = &params[2];
+
+    let function_expr = &params[3];
+
+    let function_closure: ExprClosure = parse2(function_expr.to_token_stream())
+        .expect("the third parameter of add_test! must be a closure");
+
+    let (dep_getters, _dep_names, bindings) =
+        get_dependency_params_for_closure(function_closure.inputs.iter());
+    let is_async = matches!(&*function_closure.body, Expr::Async(_));
+
+    let result = if is_async {
+        let mut lets = Vec::new();
+        for (getter, ident) in dep_getters.iter().zip(bindings) {
+            lets.push(quote! {
+                let #ident = #getter;
+            });
+        }
+        let body = match &*function_closure.body {
+            Expr::Async(inner) => inner.block.clone(),
+            _ => panic!("Expected async block"),
+        };
+        quote! {
+            #dtr_expr.add_async_test(#name_expr, #test_props_expr, move |__test_r_deps_arg| {
+                Box::pin(async move {
+                    #(#lets)*
+                    #body
+                })
+            });
+        }
+    } else {
+        quote! {
+            #dtr_expr.add_sync_test(#name_expr, #test_props_expr, move |__test_r_deps_arg| {
+                let gen_fn = #function_closure;
+                gen_fn(#(#dep_getters),*)
+            });
+        }
+    };
+
+    result.into()
+}
---
+++ test-r-macro/src/suite.rs
@@ -0,0 +1,146 @@
+use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::punctuated::Punctuated;
+use syn::{ItemMod, Token, parse_macro_input};
+
+pub fn tag(attr: TokenStream, item: TokenStream) -> TokenStream {
+    if let Ok(ast) = syn::parse::<ItemMod>(item.clone()) {
+        let random = rand::random::<u64>();
+        let register_ident = Ident::new(
+            &format!("test_r_register_mod_{}_tag_{}", ast.ident, random),
+            Span::call_site(),
+        );
+
+        let mod_name_str = ast.ident.to_string();
+
+        let tag = parse_macro_input!(attr as Ident);
+        let tag_str = tag.to_string();
+        let tag = quote! { #tag_str.to_string() };
+
+        let register_call = quote! {
+              test_r::core::register_suite_tag(
+                  #mod_name_str,
+                  module_path!(),
+                  #tag
+              );
+        };
+
+        let result = quote! {
+            #[cfg(test)]
+            #[test_r::ctor::ctor(crate_path=::test_r::ctor)]
+            fn #register_ident() {
+                 #register_call
+            }
+
+            #ast
+        };
+
+        result.into()
+    } else {
+        // applied to a test function
+        item
+    }
+}
+
+pub fn tag_suite(input: TokenStream) -> TokenStream {
+    let params = parse_macro_input!(input with Punctuated::<Ident, Token![,]>::parse_terminated);
+
+    if params.len() != 2 {
+        panic!(
+            "tag_suite! expects exactly 2 identifiers as parameters: the name of the suite module and the tag"
+        );
+    }
+
+    let mod_name_str = params[0].to_string();
+    let tag_str = params[1].to_string();
+
+    let random = rand::random::<u64>();
+    let register_ident = Ident::new(
+        &format!("test_r_register_mod_{}_tag_{}", mod_name_str, random),
+        Span::call_site(),
+    );
+
+    let tag = quote! { #tag_str.to_string() };
+
+    let register_call = quote! {
+          test_r::core::register_suite_tag(
+              #mod_name_str,
+              module_path!(),
+              #tag
+          );
+    };
+
+    let result = quote! {
+        #[cfg(test)]
+        #[test_r::ctor::ctor(crate_path=::test_r::ctor)]
+        fn #register_ident() {
+             #register_call
+        }
+    };
+
+    result.into()
+}
+
+pub fn sequential(item: TokenStream) -> TokenStream {
+    let ast: ItemMod = syn::parse(item).expect("#[sequential] must be applied to a module");
+
+    let register_ident = Ident::new(
+        &format!("test_r_register_mod_{}_sequential", ast.ident),
+        Span::call_site(),
+    );
+
+    let mod_name_str = ast.ident.to_string();
+    let register_call = quote! {
+          test_r::core::register_suite_sequential(
+              #mod_name_str,
+              module_path!(),
+          );
+    };
+
+    let result = quote! {
+        #[cfg(test)]
+        #[test_r::ctor::ctor(crate_path=::test_r::ctor)]
+        fn #register_ident() {
+             #register_call
+        }
+
+        #ast
+    };
+
+    result.into()
+}
+
+pub fn sequential_suite(input: TokenStream) -> TokenStream {
+    let params = parse_macro_input!(input with Punctuated::<Ident, Token![,]>::parse_terminated);
+
+    if params.len() != 1 {
+        panic!(
+            "sequential_suite! expects exactly 1 identifier as parameter: the name of the suite module"
+        );
+    }
+
+    let mod_name_str = params[0].to_string();
+
+    let register_ident = Ident::new(
+        &format!("test_r_register_mod_{}_sequential", mod_name_str),
+        Span::call_site(),
+    );
+
+    let register_call = quote! {
+          test_r::core::register_suite_sequential(
+              #mod_name_str,
+              module_path!(),
+          );
+    };
+
+    let result = quote! {
+        #[cfg(test)]
+        #[test_r::ctor::ctor(crate_path=::test_r::ctor)]
+        fn #register_ident() {
+             #register_call
+        }
+    };
+
+    result.into()
+}
