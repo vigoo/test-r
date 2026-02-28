d721b56937531d48d0e7113e5c9c4c3751b2c483
vigoo <vigoo@users.noreply.github.com>
2026-02-28 09:25:44 +0000

    deploy: 578290a110d3d6b247958e5dd2a4f0098d5e3bae

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4589b218.js',
+                path_to_root + 'searchindex-7337a8b6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
